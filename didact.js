8a4552273e84d26f522a2331cf9808713bbbf067
ZhiyuanG <zyguo1230@126.com>
2018-05-23 22:10:21 +0800

    Add reconcile process

--- didact.js
+++ didact.js
@@ -1,5 +1,85 @@
 const TEXT_ELEMENT = "TEXT ELEMENT";
 
+let rootInstance = null;
+
+function render(element, container) {
+    const prevInstance = rootInstance;
+    const nextInstance = reconcile(container, prevInstance, element);
+    rootInstance = nextInstance;
+}
+
+function reconcile(parentDom, instance, element) {
+    if (instance == null) {
+        const newInstance = instantiate(element);
+        parentDom.appendChild(newInstance.dom);
+        return newInstance;
+    } else if (instance.element.type === element.type) {
+        updateDomProperties(instance.dom, instance.element.props, element.props);
+        instance.childInstances = reconcileChildren(instance, element);
+        instance.element = element;
+        return instance;
+    } else {
+        const newInstance = instantiate(element);
+        parentDom.replaceChild(newInstance.dom, instance.dom);
+        return newInstance;
+    }
+}
+
+function reconcileChildren(instance, element) {
+    const dom = instance.dom;
+    const childInstances = instance.childInstances;
+    const nextChildElements = element.props.children || [];
+    const newChildInstances = [];
+    const count = Math.max(childInstances.length, nextChildElements.length);
+    for (let i = 0; i < count; i++) {
+        const childInstance = childInstances[i];
+        const childElement = nextChildElements[i];
+        const newChildInstance = reconcile(dom, childInstance, childElement);
+        newChildInstances.push(newChildInstance);
+    }
+    return newChildInstances.filter(instance => instance != null);
+}
+
+function instantiate(element) {
+    const { type, props } = element;
+
+    const isTextElement = type === "TEXT ELEMENT";
+    const dom = isTextElement
+        ? document.createTextNode("")
+        : document.createElement(type);
+
+    updateDomProperties(dom, [], props);
+
+    const childElements = props.children || [];
+    const childInstances = childElements.map(instantiate);
+    const childDoms = childInstances.map(childInstance => childInstance.dom);
+    childDoms.forEach(childDom => dom.appendChild(childDom));
+    return { element, dom, childInstances };
+}
+
+function updateDomProperties(dom, prevProps, nextProps) {
+    const isEvent = name => name.startsWith("on");
+    const isAttribute = name => !isEvent(name) && name != "children";
+    
+    Object.keys(prevProps).filter(isEvent).forEach(name => {
+        const eventType = name.toLowerCase().substring(2);
+        dom.removeEventListener(eventType, prevProps[name]);
+    });
+
+    Object.keys(prevProps).filter(isAttribute).forEach(name => {
+        dom[name] = null;
+    });
+
+    Object.keys(nextProps).filter(isEvent).forEach(name => {
+        const eventType = name.toLowerCase().substring(2);
+        dom.addEventListener(eventType, nextProps[name]);
+    });
+
+    Object.keys(nextProps).filter(isAttribute).forEach(name => {
+        dom[name] = nextProps[name];
+    });
+}
+
 function createElement(type, config, ...args) {
     const props = Object.assign({}, config);
     const hasChildren = args.length > 0;
@@ -13,28 +93,6 @@ function createElement(type, config, ...args) {
 
 function createTextElement(value) {
     return createElement(TEXT_ELEMENT, { nodeValue: value });
-}
-
-function render(element, parentDom) {
-    const { type, props } = element;
-
-    const isTextElement = type === TEXT_ELEMENT;
-    const dom = isTextElement ? document.createTextNode("") : document.createElement(type);
-    
-    const isListener = name => name.startsWith("on");
-    Object.keys(props).filter(isListener).forEach(name => {
-        const eventType = name.toLowerCase().substring(2);
-        dom.addEventListener(eventType, props[name]);
-    });
-
-    const isAttribute = name => !isListener(name) && name != "children";
-    Object.keys(props).filter(isAttribute).forEach(name => {
-        dom[name] = props[name];
-    });
-    
-    const childElements = props.children || [];
-    childElements.forEach(childElement => render(childElement, dom));
-    parentDom.appendChild(dom);
 }
 
 export default { createElement, render };
